ba4d8292fe47ff4ff0ad4b4727f736066b47d645
marena <marenmatavao782@gmail.com>
2024-11-08 16:18:38 -0500

    Just did some of the title code and implemented random into the code!

--- simpleGame.java
+++ simpleGame.java
@@ -1,3 +1,14 @@
+'This will help with the random shapes!'
+import java.util.Random;
+
 'Basically the start of our game will go here'
 public class simpleGame {
+
+    'This will be the potential title screen!'
+    public TetrisTitleScreen() {
+        setTitle("TETRIS");
+        setSize(600,400); 'Let us just see what this may look like for now'
+
+    }
 }
+
