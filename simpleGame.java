0cd38f0226965544e21ab9213888a518b03f950d
marena <marenmatavao782@gmail.com>
2024-11-08 16:42:58 -0500

    Some more code for the title screen and options for the main menu like start and exit! Also tried to do the title screen a little better!

--- simpleGame.java
+++ simpleGame.java
@@ -1,14 +1,35 @@
 'This will help with the random shapes!'
 import java.util.Random;
+import java.util.Scanner;
 
 'Basically the start of our game will go here'
 public class simpleGame {
 
-    'This will be the potential title screen!'
-    public TetrisTitleScreen() {
-        setTitle("TETRIS");
-        setSize(600,400); 'Let us just see what this may look like for now'
+    private static Random random = new Random();
+    private static int[] fallingBlocksY = new int[10];'An array for simulating falling blocks'
+    private static String[] menuOptions = {"Start","Exit"};
+    private static int selectedOption = 0;
 
+    public static void main(String[] args) {
+        'Start the game loop by showing the Tetris title screen'
+         showTitleScreen();
+    }
+
+    public static void showTitleScreen() {
+        displayTitle(); 'Displays the title logo'
+        displayMenu(); 'Displays the menu'
+    }
+
+    public static void displayMenu() {
+        System.out.println();
+        System.out.println("Choose an option: ");
+        for (int i = 0; i < menuOptions.length; i++) {
+            if (i == selectedOption) {
+                System.out.println("> " + menuOptions[i]); 'Highlights the selected option'
+            } else {
+                System.out.println(" " + menuOptions[i]);
+            }
+        }
     }
 }
 
