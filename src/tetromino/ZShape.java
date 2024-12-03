3e2cdc1642ecde65521635a3ffdadf784d2b3cb1
Elphedor <tom.tran1@ucalgary.ca>
2024-12-03 13:40:55 -0700

    Updated Tetromino package Added rotation logic to tetromino class

--- src/tetromino/ZShape.java
+++ src/tetromino/ZShape.java
@@ -1,22 +1,12 @@
 package tetromino;
 
 public class ZShape extends Tetromino {
-    private static final int[][] shape = {
+    private static final int[][] initial_shape = {
             {1, 1, 0},
             {0, 1, 1}
     };
 
     public ZShape() {
-        super(TetrominoType.Z, shape);
-    }
-
-    @Override
-    public void rotateClockwise() {
-        // Insert rotation logic here
-    }
-
-    @Override
-    public void rotateCounterClockwise() {
-        // Insert rotation logic here
+        super(TetrominoType.Z, initial_shape);
     }
 }
