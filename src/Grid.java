e7c63403f580eef3ac17074b450178ff00397543
marena <marenmatavao782@gmail.com>
2024-12-03 12:35:52 -0500

    in grid.java made a class that locks the piece into the grid when it reaches the bottom!

--- src/Grid.java
+++ src/Grid.java
@@ -3,14 +3,31 @@ public class Grid {
     public static boolean canMove(int[][] piece, int newX, int newY, int[][] grid) {
         for (int i = 0; i < piece.length; i++) {
             for (int j = 0; j < piece[i].length; j++) {
-            // Checks if the piece has a block at this position
+                // Checks if the piece has a block at this position
                 if (piece[i][j] == 1) {
                     int x = newX + j;
                     int y = newY + i;
                     if (x < 0 || x >= Main.WIDTH || y >= Main.HEIGHT || grid[x][y] == 1) {
                         return false; // Can't move if the position is out of bounds or occupied
                     }
+                }
+            }
+        }
+        return true; // Return true if the piece can move to the new position!
+    }
+
+    // Lock the piece into the grid when it reaches the bottom
+    public static void lockPiece(Tetromino piece, int x, int y int[][] grid) {
+        int[][] shape = piece.getShape(); // Get the shapes piece!
+        for (int i = 0; i < shape.length; i++) {
+            for (int j = 0; j < shape[i].length; j++) {
+                if (shape[i][j] == 1) {
+                    // For every block of the piece lock it into the grid
+                    grid[y + i][x + j] = 1; // set the corresponding grid cell to 1 (occupied)
+                }
             }
         }
     }
+
+
 }
